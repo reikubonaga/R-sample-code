e6f8d579ce845185cb7d7476ccd54628fde5e08b
rei kubonaga <kubonagarei@gmail.com>
2015-11-05 19:26:25 +0900

    Add setup.r

--- setup.r
+++ setup.r
@@ -1,5 +1,11 @@
 # RCurl = webサイトからデータをダウンロード
 install.packages('RCurl')
+# Gplot = グラフを書くツール
+install.packages("gplots")
+
 library(RCurl)
+x <- getURL("https://raw.githubusercontent.com/reikubonaga/R-sample-code/master/table.csv")
+y <- read.csv(text = x)
+
 
 
